9e7c37ef79be3990c733f889e8cab1704dad1fd2
MahmoudAhmed2003 <mahmoud2580mahmoud@gmail.com>
2024-05-24 02:23:09 +0300

    splashScreen #6

---
+++ lib/Tabs/Home Drawer/splashScreen.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+import 'package:be_safe3/HomeScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:be_safe3/Sign_in/Login.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const String routName = 'splashScreen';
+
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  bool isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      if (isLoggedIn) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Loading...',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.blue,
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
